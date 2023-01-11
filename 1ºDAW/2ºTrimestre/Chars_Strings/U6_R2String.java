dac248ff8f7a94fa01faf371460081b01a8e7d73
Sasak1tsune <juanpapereass@gmail.com>
2023-01-11 12:42:40 +0100

    Programa Random

---
+++ 1ºDAW/2ºTrimestre/Chars_Strings/U6_R2String.java
@@ -0,0 +1,16 @@
+/*___________________________________________\n* Bienvenido pibe, selecciona algúna opción*\n*                                          *\n*    Opción 1: ____________________________*\n*    Opción 2: ____________________________*\n*    Opción 3: ____________________________*\n*__________________________________________**/
+import java.util.Scanner;
+public class U6_R2String{
+    public static void main(String[]args) {
+    //Defino variables
+
+    Scanner entrada = new Scanner(System.in);
+    
+
+    //Programa principal
+    
+    }
+public static void mostrarMenu() {
+    System.out.println("____________________________________________\n* Bienvenido pibe, selecciona algúna opción*\n*                                          *\n*    Opción 1: ____________________________*\n*    Opción 2: ____________________________*\n*    Opción 3: ____________________________*\n*__________________________________________*");
+}    
+}
