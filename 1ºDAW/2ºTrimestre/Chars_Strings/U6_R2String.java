71def0d6d42068f050e5ed054a44b8b1c01ba174
Sasak1tsune <juanpapereass@gmail.com>
2023-01-12 13:34:53 +0100

    falta corregir el segundo

--- 1ºDAW/2ºTrimestre/Chars_Strings/U6_R2String.java
+++ 1ºDAW/2ºTrimestre/Chars_Strings/U6_R2String.java
@@ -14,9 +14,13 @@ public class U6_R2String{
         seleccionMenu= entrada.nextByte();
         switch(seleccionMenu){
             case 1:
-                leng
+                String frase1 = entrada.next();
+                String frase2 = entrada.next();
+                compararStrings(frase1, frase2);
                 break;
             case 2:
+                String contado = entrada.next();
+                contarEspacios(contado);
                 break;
             case 3:
                 break;
@@ -32,7 +36,7 @@ public class U6_R2String{
     }while(salirMenu==true);
     }
 public static void mostrarMenu() {
-    System.out.println("____________________________________________\n* Bienvenido pibe, selecciona algúna opción*\n*                                          *\n*    Opción 1: ____________________________*\n*    Opción 2: ____________________________*\n*    Opción 3: ____________________________*\n*__________________________________________*");
+    System.out.println("____________________________________________\n* Bienvenido pibe, selecciona algúna opción*\n*                                          *\n*    Opción 1: Comparación de carácteres   *\n*    Opción 2: ____________________________*\n*    Opción 3: ____________________________*\n*__________________________________________*");
 }
 public static boolean confirmarSalida(char x){
     Character.toLowerCase(x);
@@ -41,5 +45,30 @@ public static boolean confirmarSalida(char x){
     }else{
         return false;
     }
-}    
+}
+public static void contarEspacios(String contado){
+    int tamaño = contado.length();
+    int i = 0;
+    int espacios = 0;
+    while(tamaño!=i){ 
+        char letra = contado.charAt(i);
+        boolean esEspacio=Character.isWhitespace(letra);
+        if(esEspacio==true){
+        espacios++;
+        }
+        i++;
+    }System.out.println(espacios);
+} 
+public static void compararStrings(String string1, String string2){
+    int frase1 = string1.length();
+    int frase2 = string2.length();
+    if(frase1 > frase2){
+        System.out.println("La frase:\n"+string1+"\nes más grande que\n"+string2);
+    }else if(frase1==frase2){
+        System.out.println("Las dos frases tienen la misma cantidad de carácteres");
+    }else{
+        System.out.println("La frase:\n"+string2+"\nes más grande que\n"+string1);
+    }
+}
+
 }
