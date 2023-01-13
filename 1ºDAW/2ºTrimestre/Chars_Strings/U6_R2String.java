2c852c08565ea05328dc8f52c9b235ff618856b3
Sasak1tsune <juanpapereass@gmail.com>
2023-01-13 12:44:45 +0100

    xd

--- 1ºDAW/2ºTrimestre/Chars_Strings/U6_R2String.java
+++ 1ºDAW/2ºTrimestre/Chars_Strings/U6_R2String.java
@@ -14,15 +14,17 @@ public class U6_R2String{
         seleccionMenu= entrada.nextByte();
         switch(seleccionMenu){
             case 1:
-                String frase1 = entrada.next();
-                String frase2 = entrada.next();
+                String frase1 = peticionCadena();
+                String frase2 = peticionCadena();
                 compararStrings(frase1, frase2);
                 break;
             case 2:
-                String contado = entrada.next();
-                contarEspacios(contado);
+                String contadoEspacios = peticionCadena();
+                contarEspacios(contadoEspacios);
                 break;
             case 3:
+                String contadoFrases= peticionCadena();
+                contarEspacios(contadoFrases);
                 break;
             case 0:
                 System.out.println("Seguro que quiere salir?\n(s/n)");
@@ -50,14 +52,17 @@ public static void contarEspacios(String contado){
     int tamaño = contado.length();
     int i = 0;
     int espacios = 0;
-    while(tamaño!=i){ 
-        char letra = contado.charAt(i);
-        boolean esEspacio=Character.isWhitespace(letra);
-        if(esEspacio==true){
-        espacios++;
+    
+    do{ 
+        char caracter=contado.charAt(i);
+        if(caracter == ' '){
+            espacios++;
+            i++;
+        }else{
+            i++;
         }
-        i++;
-    }System.out.println(espacios);
+    }while(tamaño>i);
+    System.out.println(espacios);
 } 
 public static void compararStrings(String string1, String string2){
     int frase1 = string1.length();
@@ -70,5 +75,26 @@ public static void compararStrings(String string1, String string2){
         System.out.println("La frase:\n"+string2+"\nes más grande que\n"+string1);
     }
 }
-
+public static String peticionCadena(){
+    Scanner entrada = new Scanner(System.in);
+    String cadenaPedida=("");
+    cadenaPedida = entrada.nextLine();
+    return cadenaPedida;
+}
+public static void contarFrases(String contado){
+    int tamaño = contado.length();
+    int i = 0;
+    int frases = 0;
+    
+    do{ 
+        char caracter=contado.charAt(i);
+        if(caracter == ' '){
+            frases++;
+            i++;
+        }else{
+            i++;
+        }
+    }while(tamaño>i);
+    System.out.println(frases);
+} 
 }
