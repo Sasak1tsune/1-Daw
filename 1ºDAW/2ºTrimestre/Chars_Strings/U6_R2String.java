d07ebe9bad66a51f1fcf1bac4f0be7a27f1d89e2
Sasak1tsune <juanpapereass@gmail.com>
2023-01-13 14:07:39 +0100

    me falta un ejercicio

--- 1ºDAW/2ºTrimestre/Chars_Strings/U6_R2String.java
+++ 1ºDAW/2ºTrimestre/Chars_Strings/U6_R2String.java
@@ -13,19 +13,28 @@ public class U6_R2String{
         mostrarMenu();
         seleccionMenu= entrada.nextByte();
         switch(seleccionMenu){
-            case 1:
+            case 1://Comparar frases strings
                 String frase1 = peticionCadena();
                 String frase2 = peticionCadena();
                 compararStrings(frase1, frase2);
                 break;
-            case 2:
+            case 2://contar espacios
                 String contadoEspacios = peticionCadena();
                 contarEspacios(contadoEspacios);
                 break;
-            case 3:
+            case 3://Contar palabras
                 String contadoFrases= peticionCadena();
-                contarEspacios(contadoFrases);
+                contarFrases(contadoFrases);
                 break;
+            case 4://Invertir cadena
+                break;
+            case 5:
+                break;
+            case 6://INDEXOF
+                String frase = peticionCadena();
+                String subfrase = peticionCadena();
+                buscarSubcadena(frase, subfrase);
+                break;    
             case 0:
                 System.out.println("Seguro que quiere salir?\n(s/n)");
                 char salida = entrada.next().charAt(0);
@@ -37,6 +46,12 @@ public class U6_R2String{
         }               
     }while(salirMenu==true);
     }
+public static void buscarSubcadena(String frase, String subfrase){
+    int pos = frase.indexOf(subfrase);
+    if (pos>=0){
+        System.out.println("La frase "+frase+" Contiene la subfrase "+subfrase+" y está en la posición "+pos);
+    }
+};
 public static void mostrarMenu() {
     System.out.println("____________________________________________\n* Bienvenido pibe, selecciona algúna opción*\n*                                          *\n*    Opción 1: Comparación de carácteres   *\n*    Opción 2: ____________________________*\n*    Opción 3: ____________________________*\n*__________________________________________*");
 }
@@ -85,7 +100,6 @@ public static void contarFrases(String contado){
     int tamaño = contado.length();
     int i = 0;
     int frases = 0;
-    
     do{ 
         char caracter=contado.charAt(i);
         if(caracter == ' '){
@@ -95,6 +109,6 @@ public static void contarFrases(String contado){
             i++;
         }
     }while(tamaño>i);
-    System.out.println(frases);
+    System.out.println(frases+1);
 } 
 }
